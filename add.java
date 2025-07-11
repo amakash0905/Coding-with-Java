f16efd4bbc343f4e2eaa5c479f6978b63ea7561e
amakash0905 <akash.lawrencian2020@gmail.com>
2025-07-11 16:08:37 +0530

    Second prog commited

---
+++ add.java
@@ -0,0 +1,10 @@
+public class add {
+    public static void main(String[] args) {
+        int a=56;
+        int b=98;
+        int c=84;
+        int sum=a+b+c;
+        System.out.println(sum);
+
+    }
+}
